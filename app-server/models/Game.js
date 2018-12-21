887b82629019523e396dc0ce847374e384ea389a
jovenne <jovenneregs@gmail.com>
2018-12-21 14:01:16 +0800

    updated schema

--- app-server/models/Game.js
+++ app-server/models/Game.js
@@ -8,5 +8,3 @@ const gameSchema = new Schema({
 
 mongoose.model('games', gameSchema);
 
-// review this video - .save()
-// https://www.udemy.com/node-with-react-fullstack-web-development/learn/v4/t/lecture/7603034?start=0
