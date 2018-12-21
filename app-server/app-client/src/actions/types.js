7e2ff333d422ce33414a3a5d411e4af9704814d9
jovenne <jovenneregs@gmail.com>
2018-12-21 14:11:49 +0800

    redux type

---
+++ app-server/app-client/src/actions/types.js
@@ -0,0 +1,9 @@
+export const FETCH_ALL_INFOS = 'FETCH_ALL_INFOS';
+export const FETCH_SINGLE_INFO = 'FETCH_SINGLE_INFO';
+export const FETCHED_INFO = 'FETCHED_INFO';
+export const INFO_DELETED = 'INFO_DELETED';
+export const INFO_UPDATED = 'INFO_UPDATED';
+export const ADD_INFO = 'ADD_INFO';
+
+
+
